45c6ddde23e4395bd17c1cfa7da9d11b08133ecb
akmalaqim74 <akmalaqim74@gmail.com>
2025-12-31 18:15:38 +0800

    main

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -4,6 +4,6 @@ export const environment = {
     API_KEY: '123456789',
     CSV_BASE: '/csv',
     NOTION_CLIENT_ID: '',
-    LICENSE_API_URL: 'https://your-production-api.com/api/v1'
+    LICENSE_API_URL: 'https://license-api-1076822324573.asia-southeast1.run.app/api/v1'
   };
   
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,6 +3,6 @@ export const environment = {
     API_URL: 'http://localhost:8081',
     API_KEY: '123456789',
     NOTION_CLIENT_ID: '',
-    LICENSE_API_URL: 'http://localhost:3000/api/v1'
+    LICENSE_API_URL: 'https://license-api-1076822324573.asia-southeast1.run.app/api/v1'
   };
   
